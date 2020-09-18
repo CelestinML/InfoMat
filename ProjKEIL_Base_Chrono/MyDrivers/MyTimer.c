830712e408386c9e2c10988f8c050814804b6bb4
Célestin Moënne-Loccoz <celestin.moenne@gmail.com>
2020-09-18 08:03:52 +0200

    Init du projet

---
+++ ProjKEIL_Base_Chrono/MyDrivers/MyTimer.c
@@ -0,0 +1,35 @@
+// TOUT A FAIRE !! //
+
+/*
+ indispensable pour pouvoir adresser les registres des périphériques.
+ Rem : OBLIGATION d'utiliser les définitions utiles contenues dans ce fichier (ex : TIM_CR1_CEN, RCC_APB1ENR_TIM2EN ...)
+ pour une meilleure lisibilité du code.
+
+ Pour les masques, utiliser également les définitions proposée
+ Rappel : pour mettre à 1  , reg = reg | Mask (ou Mask est le représente le ou les bits à positionner à 1)
+				  pour mettre à 0  , reg = reg&~ Mask (ou Mask est le représente le ou les bits à positionner à 0)
+ 
+*/ 
+#include "stm32f103xb.h" 
+
+void MyTimer_Conf(TIM_TypeDef * Timer,int Arr, int Psc) {
+	//Activation de l'horloge
+	if (Timer == TIM1) {
+		RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+	}
+	else if (Timer == TIM2) {
+		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	}
+	else if (Timer == TIM3) {
+		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	}
+	else if (Timer == TIM4) {
+		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+	}
+	Timer->PSC = Psc;
+	Timer->ARR = Arr;
+}
+
+void MyTimer_Start(TIM_TypeDef * Timer) {
+	Timer->CR1 |= TIM_CR1_CEN;
+}
